d1ee5ccfb6620a62ed7bf03aec1ee86c57dcee86
AlirezaRahmati <35702886+alireza0r@users.noreply.github.com>
2024-02-19 22:24:00 +0330

    Update Preprocessing.py
    Add do_local_process() function

--- Libs/Preprocessing.py
+++ Libs/Preprocessing.py
@@ -13,3 +13,17 @@ def do_preprocess():
   print('OneHot shape:', onehot_label.shape)
 
   return raw, mels, onehot_label, label, length
+
+def do_local_preprocess(wav_path, sr, label_loc, label_dict):
+  raw, label, length = load_local(wav_path, sr, label_loc, label_dict)
+  mels = mel_extract(raws=raw, max_size=max(length))
+
+  max_label = np.max(label)
+  min_label = np.min(label)
+  print('Max of label:', max_label, 'Min of Label:', min_label)
+
+  onehot_label = [[1 if (i+min_label)==l else 0 for i in range(max_label)] for l in label]
+  onehot_label = np.array(onehot_label)
+  print('OneHot shape:', onehot_label.shape)
+
+  return raw, mels, onehot_label, label, length
