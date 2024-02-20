6c9c427355710b6df99077ac58a19c6e06c7b125
AlirezaRahmati <35702886+alireza0r@users.noreply.github.com>
2024-02-20 19:52:23 +0330

    Update Preprocessing.py
    problem in the number of label

--- Libs/Preprocessing.py
+++ Libs/Preprocessing.py
@@ -22,7 +22,7 @@ def do_local_preprocess(wav_path, sr, label_loc, label_dict):
   min_label = np.min(label)
   print('Max of label:', max_label, 'Min of Label:', min_label)
 
-  onehot_label = [[1 if (i+min_label)==l else 0 for i in range(max_label)] for l in label]
+  onehot_label = [[1 if i==l else 0 for i in range(min_label, max_label+1)] for l in label]
   onehot_label = np.array(onehot_label)
   print('OneHot shape:', onehot_label.shape)
 
